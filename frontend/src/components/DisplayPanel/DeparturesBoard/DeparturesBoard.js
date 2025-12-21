2b8bef6d4b02036b8ebb4acf0126a1a252b9e7de
nemkevichgeorg <nemkevichgeorg@yandex.ru>
2025-12-21 22:12:02 +0300

    12309

--- frontend/src/components/DisplayPanel/DeparturesBoard/DeparturesBoard.js
+++ frontend/src/components/DisplayPanel/DeparturesBoard/DeparturesBoard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { API } from '../../services/api';
 
 function DeparturesBoard() {
   const [flights, setFlights] = useState([]);
